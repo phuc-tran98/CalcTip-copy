187be4788b12949b29d7cf4f1b532b91d8a8b63f
Phuc Tran <cuhptran@gmail.com>
2020-08-01 15:08:19 -0700

    Initial Commit

---
+++ CalcTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcTip
+//
+//  Created by PaulWall on 8/1/20.
+//  Copyright © 2020 Phuc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
